e846391ecb4104e5de68d5c697c1a1a93a854911
leenawat papahom <leenawat.pa@gmail.com>
2024-01-09 21:31:33 +0700

    init my spring-boot-example

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # spring-boot-example
+
+```
+$ ./mvnw install
+$ ./mvnw spring-boot:run -pl application
+```
+change "application" to "your app name"
---
+++ spring-crud-user-role/src/main/java/com/punyadev/springcruduserrole/SpringCrudUserRoleApplication.java
@@ -0,0 +1,13 @@
+package com.punyadev.springcruduserrole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrudUserRoleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCrudUserRoleApplication.class, args);
+	}
+
+}
