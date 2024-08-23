e77cba88fdd5f6a5961639b17de07c2712fa401c
leenawat papahom <leenawat.pa@gmail.com>
2024-08-23 13:48:27 +0700

    add java sftp server for dev

---
+++ java-sftp-server/src/main/java/com/punyadev/JavaSftpserverApplication.java
@@ -0,0 +1,13 @@
+package com.punyadev;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSftpserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSftpserverApplication.class, args);
+        while (true);
+    }
+
+}
